e655525cfe7ea30edaeca6c023c4e8366798e8e0
Seneth <126297209+sen3th@users.noreply.github.com>
2026-05-28 20:21:32 -0400

    made types for musicbrainz response

---
+++ src/types/musicbrainz.ts
@@ -0,0 +1,16 @@
+export type MusicBrainzArtistCredit = Array<{
+    name: string;
+    artist: { id: string; name: string;};
+}>;
+
+export type MusicBrainzReleaseGroup = {
+    id: string;
+    title: string;
+    "primary-type"?: string;
+    "release-date"?: string;
+    "artist-credit": MusicBrainzArtistCredit;
+};
+
+export type MusicBrainzReleaseGroupSearchResponse = {
+    "release-groups": MusicBrainzReleaseGroup[];
+};
