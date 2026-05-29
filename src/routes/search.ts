30e64191ee4f39db40a144875610492e0b190e9d
Seneth <126297209+sen3th@users.noreply.github.com>
2026-05-29 12:18:55 -0400

    added musicbrainz error handling

--- src/routes/search.ts
+++ src/routes/search.ts
@@ -13,6 +13,25 @@ searchRouter.get("/release-groups", async (req, res) =>{
     if (!album.trim()){
         return res.status(400).json({error: "need a query parameter 'album'"});
     }
+
+    try {
+        const data = await searchReleaseGroups({ album: album.trim(), artist, limit });
+
+        const items = data["release-groups"].map((rg)=>({
+            id: rg.id,
+            title: rg.title,
+            primaryType: rg["primary-type"] ?? null,
+            artistCredit:
+                rg["artist-credit"]?.map((ac) => ({
+                    name: ac.name,
+                    artist: { id: ac.artist.id, name: ac.artist.name},
+                })) ?? [],
+        }));
+        return res.json({ items });
+    } catch (err) {
+        return res.json({ items: [], error: "music brainz error"});
+    }
+
     const data = await searchReleaseGroups({ album: album.trim(), artist, limit});
 
     const items = data["release-groups"].map((rg)=>({
