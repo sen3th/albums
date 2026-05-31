1bb157677b75c00a47212a1be47653cd680437bd
Seneth <126297209+sen3th@users.noreply.github.com>
2026-05-31 15:43:41 -0400

    added cors

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,11 @@ import express from "express";
 import {config} from "./config";
 import { searchRouter } from "./routes/search";
 import { similarRouter } from "./routes/similar";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/health", (_req, res) =>{
     res.json({ ok: true});
