754fb521aba210391777b68683f3062992851dd8
Seneth <126297209+sen3th@users.noreply.github.com>
2026-06-15 17:31:26 -0400

    added secondary types to musicbrainz releasegroup

--- src/types/musicbrainz.ts
+++ src/types/musicbrainz.ts
@@ -1,14 +1,15 @@
 export type MusicBrainzArtistCredit = Array<{
     name: string;
-    artist: { id: string; name: string;};
+    artist: { id: string; name: string };
 }>;
 
 export type MusicBrainzReleaseGroup = {
     id: string;
     title: string;
-    "primary-type"?: string;
-    "first-release-date"?: string;
+    "primary-type"?: string | null;
+    "secondary-types"?: string[];
     "artist-credit"?: MusicBrainzArtistCredit;
+    disambiguation?: string | null;
 };
 
 export type MusicBrainzReleaseGroupSearchResponse = {
